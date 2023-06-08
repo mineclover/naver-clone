8820af29d8f87fea38d9d9f363550df45a76c2ec
mineclover <mineclover@naver.com>
2023-06-09 03:22:22 +0900

    env. redux 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { Provider } from "react-redux"
-import { store } from "./app/store"
 
 import Main from "./pages/Main"
 
@@ -10,10 +8,4 @@ import "./index.css"
 import "./tokens/light.variables.css"
 import "./tokens/dark.variables.css"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Main />
-    </Provider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(<Main />)
